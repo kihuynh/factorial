ceb84502d63da9bdcf10175ef878433ed54bd406
David Lewis and Kimberly Huynh <david@>
2018-01-17 14:57:11 -0800

    initialize file and create factorial

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var num=6;
+
+if (num===1) {
+alert(num)
+}else {
+
+for (index=num-1; index>0; index-=1) {
+  num = num * index;
+}
+}
+alert(num);
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+
+
+
+
+
+
+});
